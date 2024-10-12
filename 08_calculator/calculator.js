6bdd1d8d27dc0395b4395be4e6256996c590e30a
Ben <lechiben@hotmail.com>
2024-10-11 22:21:13 -0700

    solve calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product,current) => product * current);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  let result = 1;
+  for (let i = 1; i <= exponent; i++){
+    result *= base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number == 0) return 1;
+  let result = 1;
+  for (let i = 1; i <= number; i++){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
