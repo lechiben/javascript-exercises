6a8e33db13e9c123851c79cf1f0b2f508a2530f9
Ben <lechiben@hotmail.com>
2024-10-12 14:06:00 -0700

    solve palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleanString = str
+    .toLowerCase()
+    .split('')
+    .filter((characters) => alphanumerical.includes(characters))
+    .join('');
 
+    const reverseString = cleanString.split('').reverse().join('');
+    
+    return cleanString == reverseString;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+//step 1: remove all the space in the string. 
+//step 2: check if  string[0] = string[string.length]
