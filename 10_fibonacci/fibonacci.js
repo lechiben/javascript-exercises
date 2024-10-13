e38ff3c55269cf472e432c574e2c29d51d21248d
Ben <lechiben@hotmail.com>
2024-10-12 22:40:22 -0700

    find any number in the fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+    // Create a function that returns a specific member of the Fibonacci sequence:
+    let count;
+    if (typeof countArg !== 'number'){
+        countArg = parseInt(countArg);
+        count = countArg;
+    } else {
+        count = countArg;
+    }
+
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+    
+    let firstPrev = 1;
+    let secondPrev = 0;
+    for (let i = 2; i <= count; i++){
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+    return firstPrev;
+
 
 };
 
