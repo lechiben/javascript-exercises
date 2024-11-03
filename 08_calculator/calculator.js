57d55b257e08663620a78d2eb45e4f61f742fca7
Ben <lechiben@hotmail.com>
2024-11-02 22:09:23 -0700

    colour the buttons

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,43 +1,59 @@
-const add = function(num1, num2) {
-  return num1 + num2;
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+  display.value += input;
+
+}
+
+function clearDisplay(){
+
+}
+
+function calculate(){
+
+}
+
+
+// const add = function(num1, num2) {
+//   return num1 + num2;
 	
-};
+// };
 
-const subtract = function(num1, num2) {
-	return num1 - num2;
-};
+// const subtract = function(num1, num2) {
+// 	return num1 - num2;
+// };
 
-const sum = function(array) {
-	return array.reduce((total, num) => total + num, 0);
-};
+// const sum = function(array) {
+// 	return array.reduce((total, num) => total + num, 0);
+// };
 
-const multiply = function(array) {
-  return array.reduce((product,current) => product * current);
-};
+// const multiply = function(array) {
+//   return array.reduce((product,current) => product * current);
+// };
 
-const power = function(base, exponent) {
-  let result = 1;
-  for (let i = 1; i <= exponent; i++){
-    result *= base;
-  }
-  return result;
-};
+// const power = function(base, exponent) {
+//   let result = 1;
+//   for (let i = 1; i <= exponent; i++){
+//     result *= base;
+//   }
+//   return result;
+// };
 
-const factorial = function(number) {
-  if (number == 0) return 1;
-  let result = 1;
-  for (let i = 1; i <= number; i++){
-    result *= i;
-  }
-  return result;
-};
+// const factorial = function(number) {
+//   if (number == 0) return 1;
+//   let result = 1;
+//   for (let i = 1; i <= number; i++){
+//     result *= i;
+//   }
+//   return result;
+// };
 
-// Do not edit below this line
-module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
-};
+// // Do not edit below this line
+// module.exports = {
+//   add,
+//   subtract,
+//   sum,
+//   multiply,
+//   power,
+//   factorial
+// };
